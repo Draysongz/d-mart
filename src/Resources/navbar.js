e3e506422188b9ff28bbae9751f0aa97c6d5b0ad
Ackermann <dharmiegra4@gmail.com>
2022-11-17 10:33:44 +0100

    folder

---
+++ src/Resources/navbar.js
@@ -0,0 +1,22 @@
+export const navbar = [
+    {
+        title: "Home",
+        cname: "navlink",
+        id: "#home"
+    },
+    {
+        title: "About",
+        cname: "navlink",
+        id: "#about"
+    },
+    {
+        title: "Featured",
+        cname: "navlink",
+        id: "#featured"
+    },
+    {
+        title: "Contact",
+        cname: "navlink",
+        id: "#contact"
+    }
+]
